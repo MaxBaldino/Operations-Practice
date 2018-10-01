1c72ce4d525f81b82e02130e5209daf4ed9ea454
MaxBaldino <43002640+MaxBaldino@users.noreply.github.com>
2018-10-01 08:32:36 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,27 @@
+
+public class OperationsPractice {
+
+	public static void main(String  []args) {
+		
+		int number;  //Data type, then variable name.
+		number = 7;
+		System.out.println(number);
+		number += 5;
+		System.out.print("+ 5: ");
+		System.out.println(number);
+		number /= 2;
+		System.out.print("/ 2: ");
+		System.out.println(number);
+		number *= 4;
+		System.out.print("* 4: ");
+		System.out.println(number);
+		number -= 6;
+		System.out.print("- 6: ");
+		System.out.println(number);
+		number %= 2;
+		System.out.print("% 2: ");
+		System.out.println(number);
+		
+	}
+	
+}
